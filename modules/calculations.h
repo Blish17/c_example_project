67b33d4fd8eed7185480644175568a859b7043be
Christopher Hilfing <christopher.hilfing@ost.ch>
2023-02-23 21:26:12 +0100

    working tests...

---
+++ modules/calculations.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#ifndef TESTING_FRAMEWORK_CALCULATIONS_H
+#define TESTING_FRAMEWORK_CALCULATIONS_H
+
+int calculate_sum(int arr[], size_t length);
+float calculate_mean(int arr[], size_t length);
+
+#endif //TESTING_FRAMEWORK_CALCULATIONS_H
